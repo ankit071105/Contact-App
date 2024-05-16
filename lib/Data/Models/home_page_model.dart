01535f93a586caf21de4cc20cb06fa0dda27ce7b
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2024-05-17 00:33:32 +0530

    Add files via upload

---
+++ lib/Data/Models/home_page_model.dart
@@ -0,0 +1,24 @@
+class ListData {
+  final String name;
+  final String time;
+  final String description;
+
+  ListData({
+    required this.name,
+    required this.time,
+    required this.description,
+  });
+}
+
+List<ListData> listData = [
+  ListData(name: 'Ankit Kumar', time: '03:22pm', description: 'Flutter Developer'),
+  ListData(name: 'Avnish Kumar', time: '11:12am', description: 'Hardware Designer'),
+  ListData(name: 'Himashu Kumar', time: '07:10pm', description: 'React Developer'),
+  ListData(name: 'Kalind ', time: '02:01pm', description: 'Web Developer'),
+  ListData(name: 'Ankit Kumar', time: '08:40am', description: 'Dart Developer'),
+  ListData(name: 'Avnish Kumar', time: '02:52pm', description: 'Designer'),
+  ListData(name: 'Ankit Kumar', time: '06:07pm', description: 'Swift Developer'),
+  ListData(name: 'Kalind', time: '12:11pm', description: 'Java Developer'),
+  ListData(name: 'Himashu Kumar', time: '01:12pm', description: 'Android Developer'),
+
+];
---
+++ lib/Presentation/Declarations/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color.fromARGB(255, 113, 35, 227);
+Color secondaryColor = const Color.fromARGB(255, 121, 248, 238);
+Color accentColor = const Color(0xfffff6bf);
+
+const kSpacing = 10.00;
+
+const kPadding = EdgeInsets.all(kSpacing);
+const kVPadding = EdgeInsets.symmetric(vertical: kSpacing);
+const kHPadding = EdgeInsets.symmetric(horizontal: kSpacing);
+const kSymPadding =
+    EdgeInsets.symmetric(vertical: kSpacing, horizontal: kSpacing);
+
+var kBorderRadius = BorderRadius.circular(kSpacing);
